2c519b14edd26133b8dc4fbf2025d0d6eb3f1adb
Jose Maria <melopsitaco@gmail.com>
2020-01-12 21:42:37 +0100

    calculate price

--- src/ista/device.py
+++ src/ista/device.py
@@ -1,4 +1,6 @@
 import pandas as pd
+
+
 class Device(object):
     def __init__(self, row):
         self.serial = row['serial']
@@ -17,15 +19,22 @@ class MonthlyConsumption(object):
             str(self.energy), str(self.price), str(self.month))
         return out
 
-def calc_price(consumption,last_dates):
 
-    consumption=pd.concat([consumption,last_dates])
-    consumption.drop_duplicates(keep="last",inplace=True)
-    consumption.sort_values(by="date")
-    consumption["price"]=0
-    consumption["price"]= 0
-    devices=consumption.serial.unique()
+def calc_price(consumption, last_dates, tariff):
+    consumption = pd.concat([consumption, last_dates])
+    consumption.drop_duplicates(keep="last", inplace=True)
+    consumption.sort_values(by=["date"], ascending=False,inplace=True)
+    consumption["price"] = 0
+    consumption["energy"] = 0
+    devices = consumption.serial.unique()
+    device_consumption_array = []
+
     for serial in devices:
         device_consumption = consumption[consumption.serial == serial]
-
-
+        device_consumption.energy = device_consumption.value.diff(periods=-1)
+        device_consumption.price = device_consumption.energy * tariff
+        max_date = last_dates.date[last_dates.serial == serial].values
+        if len(max_date) > 0:
+            device_consumption = device_consumption[device_consumption.date > max_date[0]]
+        device_consumption_array.append(device_consumption)
+    return pd.concat(device_consumption_array)
--- src/test/test_ista.py
+++ src/test/test_ista.py
@@ -1,4 +1,5 @@
 from ista.ista import Ista
+from device import calc_price
 import sslkeylog
 import configparser
 import pandas as pd
@@ -6,7 +7,7 @@ import pandas as pd
 
 def test_login_no_ok():
     sslkeylog.set_keylog("sslkeylog.txt")
-    ista = Ista("XXX","XXX")
+    ista = Ista("XXX", "XXX")
     out = ista.login_ista()
     assert out is False
 
@@ -24,3 +25,38 @@ def test_login_ok():
     data = ista.get_readings()
     assert data.value.mean() > 0
     assert data.date.max() == pd.Timestamp('2020-01-08 00:00:00')
+    assert data.columns.values.tolist() == ['serial', 'date', 'value']
+
+
+def test_calc_price():
+    serial1 = '11111'
+    serial2 = '22222'
+    serial3 = '33333'
+    day1 = pd.Timestamp('2020-01-01')
+    day2 = pd.Timestamp('2020-01-02')
+    day3 = pd.Timestamp('2020-01-03')
+    consumption = pd.DataFrame({'serial': [serial1, serial1, serial1, serial2, serial2, serial2, serial3, serial3],
+                                'date': [day1, day2, day3, day1, day2, day3, day1, day2],
+                                'value': [10, 11, 12, 30, 30.5, 40, 21, 22]
+                                })
+
+    last_day1 = pd.Timestamp('2019-12-31')
+    last_day2 = pd.Timestamp('2020-01-01')
+    last_date = pd.DataFrame({'serial': [serial1, serial2],
+                                'date': [last_day1, last_day2],
+                                'value': [7, 28]
+                                })
+    tariff = 2
+    price_consumption = calc_price(consumption,last_date,tariff)
+    assert price_consumption.price.mean() > 0
+    assert price_consumption.energy.mean() > 0
+    con = price_consumption[price_consumption.serial == serial1]
+    assert con.energy[con.date == day3].values.tolist() == [1]
+    assert con.energy[con.date == day2].values.tolist() == [1]
+    assert con.energy[con.date == day1].values.tolist() == [3]
+    assert con.price[con.date == day1].values.tolist() == [6]
+    con = price_consumption[price_consumption.serial == serial2]
+    assert con.energy[con.date == day3].values.tolist() == [9.5]
+    assert con.energy[con.date == day2].values.tolist() == [0.5]
+    assert len(con.energy[con.date == day1].values) == 0
+
